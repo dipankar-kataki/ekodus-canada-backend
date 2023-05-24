431edc1d53d99aa314230212ad37eec8de99de3d
dipankar kataki <dipankar.kataki@ekodus.com>
2023-05-24 14:40:37 +0530

    dahboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -8,12 +8,11 @@
             <div class="card widget-box-three">
                 <div class="card-body">
                     <div class="float-right mt-2">
-                        <i class="mdi mdi-chart-areaspline display-3 m-0"></i>
+                        <i class="mdi mdi-sitemap display-3 m-0"></i>
                     </div>
                     <div class="overflow-hidden">
-                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Statistics</p>
-                        <h2 class="mb-0"><span data-plugin="counterup">34578</span> <i class="mdi mdi-arrow-up text-success font-24"></i></h2>
-                        <p class="text-muted mt-2 m-0"><span class="font-weight-medium">Last:</span> 30.4k</p>
+                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Total Services</p>
+                        <h2 class="mb-0"><span data-plugin="counterup">{{$total_services}}</span></h2>
                     </div>
                 </div>
             </div>
@@ -24,12 +23,11 @@
             <div class="card widget-box-three">
                 <div class="card-body">
                     <div class="float-right mt-2">
-                        <i class="mdi mdi-account-convert display-3 m-0"></i>
+                        <i class="mdi mdi-file-document-outline display-3 m-0"></i>
                     </div>
                     <div class="overflow-hidden">
-                        <p class="text-uppercase font-weight-medium text-truncate mb-2">User Today</p>
-                        <h2 class="mb-0"><span data-plugin="counterup">895</span> <i class="mdi mdi-arrow-down text-danger font-24"></i></h2>
-                        <p class="text-muted mt-2 m-0"><span class="font-weight-medium">Last:</span> 1250</p>
+                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Resumes Recieved</p>
+                        <h2 class="mb-0"><span data-plugin="counterup">{{$total_candidates}}</span></h2>
                     </div>
 
                 </div>
@@ -44,9 +42,8 @@
                         <i class="mdi mdi-layers display-3 m-0"></i>
                     </div>
                     <div class="overflow-hidden">
-                        <p class="text-uppercase font-weight-medium text-truncate mb-2">User This Month</p>
-                        <h2 class="mb-0"><span data-plugin="counterup">52410</span><i class="mdi mdi-arrow-up text-success font-24"></i></h2>
-                        <p class="text-muted mt-2 m-0"><span class="font-weight-medium">Last:</span> 40.33k</p>
+                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Total Products</p>
+                        <h2 class="mb-0"><span data-plugin="counterup">{{$total_products}}</span></h2>
                     </div>
                 </div>
             </div>
@@ -57,246 +54,17 @@
             <div class="card widget-box-three">
                 <div class="card-body">
                     <div class="float-right mt-2">
-                        <i class="mdi mdi-av-timer display-3 m-0"></i>
+                        <i class="mdi mdi-blogger display-3 m-0"></i>
                     </div>
                     <div class="overflow-hidden">
-                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Request Per Minute</p>
-                        <h2 class="mb-0"><span data-plugin="counterup">652</span> <i class="mdi mdi-arrow-down text-danger font-24"></i></h2>
-                        <p class="text-muted mt-2 m-0"><span class="font-weight-medium">Last:</span> 956</p>
+                        <p class="text-uppercase font-weight-medium text-truncate mb-2">Total Blogs</p>
+                        <h2 class="mb-0"><span data-plugin="counterup">{{$total_blogs}}</span></h2>
                     </div>
 
                 </div>
             </div>
         </div>
         <!-- end col -->
-    </div>
-    <!-- end row -->
-
-    <div class="row">
-        <div class="col-xl-4">
-            <div class="card-box">
-
-                <h4 class="header-title mb-4">Daily Sales</h4>
-
-                <div class="widget-chart text-center">
-                    <div id="morris-donut-example" class="morris-charts" style="height: 245px;"></div>
-                    <ul class="list-inline chart-detail-list mb-0">
-                        <li class="list-inline-item">
-                            <h6 class="text-danger"><i class="fa fa-circle mr-2"></i>Series A</h6>
-                        </li>
-                        <li class="list-inline-item">
-                            <h6 class="text-success"><i class="fa fa-circle mr-2"></i>Series B</h6>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!-- end col -->
-
-        <div class="col-xl-4">
-            <div class="card-box">
-
-                <h4 class="header-title mb-4">Statistics</h4>
-                <div id="morris-bar-example" class="text-center morris-charts" style="height: 280px;"></div>
-            </div>
-        </div>
-        <!-- end col -->
-
-        <div class="col-xl-4">
-            <div class="card-box">
-
-                <h4 class="header-title mb-4">Total Revenue</h4>
-                <div id="morris-line-example" class="text-center morris-charts" style="height: 280px;"></div>
-            </div>
-        </div>
-        <!-- end col -->
-
-    </div>
-    <!-- end row -->
-
-    <div class="row">
-        <div class="col-xl-6">
-            <div class="card-box">
-                <h4 class="header-title mb-4">Recent Users</h4>
-
-                <div class="table-responsive">
-                    <table class="table table-hover table-centered m-0">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>User Name</th>
-                                <th>Phone</th>
-                                <th>Location</th>
-                                <th>Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>
-                                    <img src="assets/images/users/avatar-1.jpg" alt="user" class="avatar-sm rounded-circle" />
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Louis Hansen</h5>
-                                    <p class="m-0 text-muted"><small>Web designer</small></p>
-                                </td>
-                                <td>+12 3456 789</td>
-                                <td>USA</td>
-                                <td>07/08/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <img src="assets/images/users/avatar-2.jpg" alt="user" class="avatar-sm rounded-circle" />
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Craig Hause</h5>
-                                    <p class="m-0 text-muted"><small>Programmer</small></p>
-                                </td>
-                                <td>+89 345 6789</td>
-                                <td>Canada</td>
-                                <td>29/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <img src="assets/images/users/avatar-3.jpg" alt="user" class="avatar-sm rounded-circle" />
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Edward Grimes</h5>
-                                    <p class="m-0 text-muted"><small>Founder</small></p>
-                                </td>
-                                <td>+12 29856 256</td>
-                                <td>Brazil</td>
-                                <td>22/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <img src="assets/images/users/avatar-4.jpg" alt="user" class="avatar-sm rounded-circle" />
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Bret Weaver</h5>
-                                    <p class="m-0 text-muted"><small>Web designer</small></p>
-                                </td>
-                                <td>+00 567 890</td>
-                                <td>USA</td>
-                                <td>20/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <img src="assets/images/users/avatar-5.jpg" alt="user" class="avatar-sm rounded-circle" />
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Mark</h5>
-                                    <p class="m-0 text-muted"><small>Web design</small></p>
-                                </td>
-                                <td>+91 123 456</td>
-                                <td>India</td>
-                                <td>07/07/2016</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-
-                </div>
-                <!-- table-responsive -->
-            </div>
-            <!-- end card -->
-        </div>
-        <!-- end col -->
-
-        <div class="col-xl-6">
-            <div class="card-box">
-                <h4 class="header-title mb-4">Recent Users</h4>
-
-                <div class="table-responsive">
-                    <table class="table table-hover table-centered m-0">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>User Name</th>
-                                <th>Phone</th>
-                                <th>Location</th>
-                                <th>Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>
-                                    <span class="avatar-sm-box bg-success">L</span>
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Louis Hansen</h5>
-                                    <p class="m-0 text-muted"><small>Web designer</small></p>
-                                </td>
-                                <td>+12 3456 789</td>
-                                <td>USA</td>
-                                <td>07/08/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <span class="avatar-sm-box bg-primary">C</span>
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Craig Hause</h5>
-                                    <p class="m-0 text-muted"><small>Programmer</small></p>
-                                </td>
-                                <td>+89 345 6789</td>
-                                <td>Canada</td>
-                                <td>29/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <span class="avatar-sm-box bg-brown">E</span>
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Edward Grimes</h5>
-                                    <p class="m-0 text-muted"><small>Founder</small></p>
-                                </td>
-                                <td>+12 29856 256</td>
-                                <td>Brazil</td>
-                                <td>22/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <span class="avatar-sm-box bg-pink">B</span>
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Bret Weaver</h5>
-                                    <p class="m-0 text-muted"><small>Web designer</small></p>
-                                </td>
-                                <td>+00 567 890</td>
-                                <td>USA</td>
-                                <td>20/07/2016</td>
-                            </tr>
-
-                            <tr>
-                                <th>
-                                    <span class="avatar-sm-box bg-orange">M</span>
-                                </th>
-                                <td>
-                                    <h5 class="m-0 font-15">Mark</h5>
-                                    <p class="m-0 text-muted"><small>Web design</small></p>
-                                </td>
-                                <td>+91 123 456</td>
-                                <td>India</td>
-                                <td>07/07/2016</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-
-                </div>
-                <!-- table-responsive -->
-            </div>
-            <!-- end card -->
-        </div>
-        <!-- end col -->
-
     </div>
     <!-- end row -->
 @endsection
