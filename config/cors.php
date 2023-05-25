8307a07d9804d9a2afa8d149488e206a59be2e4f
dipankar kataki <dipankar.kataki@ekodus.com>
2023-05-25 15:25:22 +0530

    ekodu canada login page

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['http://127.0.0.1:5173', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
